1ecf0d3bb74dcdb474f6eee40d5ad12f581b2684
beladasharrow-netizen <bela.das.harrow@gmail.com>
2026-02-10 20:15:52 +0000

    Create predict.py

---
+++ predict.py
@@ -0,0 +1,6 @@
+# New student data
+new_student = [[4, 90, 7, 80]]  
+# study_hours, attendance, sleep, previous_grade
+
+predicted_grade = model.predict(new_student)
+print("Predicted Final Grade:", predicted_grade[0])
