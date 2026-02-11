088765d8f887f8f71428bd72942420d8559b92d0
beladasharrow-netizen <bela.das.harrow@gmail.com>
2026-02-11 11:35:10 +0000

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,3 +1,9 @@
+import joblib
+import numpy as np
+
+# Load trained model
+model = joblib.load("student_model.pkl")
+
 # New student data
 new_student = [[4, 90, 7, 80]]  
 # study_hours, attendance, sleep, previous_grade
